80ebdedd129b807fe4b3a50c3a38dc7d0a57ca49
Daniel Slovinsky <slovinsky1993@gmail.com>
2021-09-03 15:08:18 -0700

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+// import { Link } from "gatsby"
+// import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+// import Seo from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <p>Hello World</p>
+  </Layout>
+)
+
+export default IndexPage
