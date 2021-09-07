44bd11e2c92a4a14ec230e8fe479d81620d760b0
Daniel Slovinsky <slovinsky1993@gmail.com>
2021-09-06 23:50:56 -0700

    Completes responsiveness for navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,18 @@
-import * as React from "react"
+import React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import { Menu, useMediaQuery } from "@material-ui/core"
 
 import Arrow from "../assets/arrow.svg"
+import Hamburger from "../assets/hamburger.svg"
 import Data from "../interfaces/Data"
 
 import * as styles from "../styles/components/navbar.module.scss"
 
 export default function NavBar() {
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement>()
+  const isDesktop = useMediaQuery("(min-width: 768px")
+
   // ideally, would have parameter for sorting, but createdAt works for now in the interest of time
   const { allContentfulNavBarLinks: data } = useStaticQuery<Data>(graphql`
     query {
@@ -20,22 +25,60 @@ export default function NavBar() {
     }
   `)
 
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(e.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(undefined)
+  }
+
   return (
     <nav className={styles.navbar_container}>
       <div className={styles.navbar}>
         <StaticImage src="../images/brackets-logo.svg" alt="Brackets Logo" />
-        <div className={styles.nav_links}>
-          {data?.nodes.map(({ linkText, isStart }) => (
-            <Link
-              key={linkText}
-              className={isStart ? styles.start : undefined}
-              to="#"
+        {isDesktop ? (
+          <div className={styles.nav_links}>
+            {data?.nodes.map(({ linkText, isStart }) => (
+              <Link
+                key={linkText}
+                className={isStart ? styles.start : undefined}
+                to="#"
+              >
+                {linkText}
+                {isStart && <Arrow />}
+              </Link>
+            ))}
+          </div>
+        ) : (
+          <div className={styles.nav_links}>
+            <button onClick={handleClick} className={styles.menu_button}>
+              <Hamburger />
+            </button>
+            <Menu
+              open={!!anchorEl}
+              onClose={handleClose}
+              anchorEl={anchorEl}
+              transitionDuration={0}
+              className={styles.menu}
             >
-              {linkText}
-              {isStart && <Arrow />}
-            </Link>
-          ))}
-        </div>
+              {data?.nodes.map(({ linkText, isStart }) => (
+                <Link
+                  key={linkText}
+                  className={`${styles.menu_item} ${
+                    isStart ? styles.start : undefined
+                  }`}
+                  to="#"
+                >
+                  <p>
+                    {linkText}
+                    {isStart && <Arrow />}
+                  </p>
+                </Link>
+              ))}
+            </Menu>
+          </div>
+        )}
       </div>
     </nav>
   )
